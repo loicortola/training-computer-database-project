1729178db715d306469c5d6ebf42dfdba6d0e6ec
lortola <lortola@excilys-lo.ebi.excilys.com>
2013-02-28 14:38:28 +0100

    DataSourceFactory

---
+++ src/com/formation/project/computerDatabase/dao/DataSourceFactory.java
@@ -0,0 +1,70 @@
+package com.formation.project.computerDatabase.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.formation.project.computerDatabase.exception.DAOConfigurationException;
+import com.jolbox.bonecp.BoneCPConfig;
+import com.jolbox.bonecp.BoneCPDataSource;
+
+public enum DataSourceFactory {
+
+    INSTANCE;
+
+    private static final String FICHIER_PROPERTIES       = "com/formation/project/computerDatabase/dao/dao.properties";
+
+    private static final String PROPERTY_URL             = "url";
+    private static final String PROPERTY_DRIVER          = "driver";
+    private static final String PROPERTY_NOM_UTILISATEUR = "user";
+    private static final String PROPERTY_MOT_DE_PASSE    = "password";
+	
+	protected BoneCPDataSource ds	 				 	 = null;
+
+    public synchronized void  initialize() {
+    	if(ds == null) {
+	    	Properties properties = new Properties();
+	        String url;
+	        String driver;
+	        String nomUtilisateur;
+	        String motDePasse;
+
+	        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	        InputStream fichierProperties = classLoader.getResourceAsStream( FICHIER_PROPERTIES );
+
+	        if ( fichierProperties == null ) {
+	            throw new DAOConfigurationException( "Cannot find properties file " + FICHIER_PROPERTIES );
+	        }
+
+	        try {
+	            properties.load( fichierProperties );
+	            url = properties.getProperty( PROPERTY_URL );
+	            driver = properties.getProperty( PROPERTY_DRIVER );
+	            nomUtilisateur = properties.getProperty( PROPERTY_NOM_UTILISATEUR );
+	            motDePasse = properties.getProperty( PROPERTY_MOT_DE_PASSE );
+	        } catch ( FileNotFoundException e ) {
+	        	throw new DAOConfigurationException( "Cannot find properties file " + FICHIER_PROPERTIES , e);
+	        } catch ( IOException e ) {
+	            throw new DAOConfigurationException( "Cannot load properties file " + FICHIER_PROPERTIES, e );
+	        }
+
+	        try {
+	            Class.forName( driver );
+	        } catch ( ClassNotFoundException e ) {
+	            throw new DAOConfigurationException( "Cannot find driver in classpath", e );
+	        }
+
+	        
+	        BoneCPConfig config = new BoneCPConfig();
+	        config.setJdbcUrl( url );
+	        config.setUsername( nomUtilisateur );
+	        config.setPassword( motDePasse );
+	        config.setMinConnectionsPerPartition( 5 );
+	        config.setMaxConnectionsPerPartition( 10 );
+	        config.setPartitionCount( 2 );
+	        
+	        ds = new BoneCPDataSource( config );
+	    }
+    }
+}
