7f96954ec28f6089a7c97361a6d38e24d0eceb1d
lortola <lortola@excilys-lo.ebi.excilys.com>
2013-03-15 14:27:52 +0100

    Refactoring of asserts:adding comments

--- backend/src/main/java/com/formation/project/computerDatabase/service/ComputerDatabaseServiceImpl.java
+++ backend/src/main/java/com/formation/project/computerDatabase/service/ComputerDatabaseServiceImpl.java
@@ -48,70 +48,56 @@ public class ComputerDatabaseServiceImpl implements IComputerDatabaseService {
 	@Transactional(readOnly=false)
 	public void addComputer(Computer computer) {
 		logger.debug("Entering Service.addComputer");
-		try {
-			Assert.notNull(computer);
-			Assert.hasText(computer.getName().trim());
-			
-			computerDao.save(computer);
-			
-			Stat stat = new Stat(computer.getId(),"add");
-			
-			statDao.save(stat);
-		} catch (IllegalArgumentException e) {
-			logger.warn("WARNING: iae in Service.addComputer: {}", e.getMessage());
-			throw new IllegalArgumentException();
-		}
+		Assert.notNull(computer,"Assert Exception: Computer is null in Service.addComputer");
+		Assert.hasText(computer.getName(),"Assert Exception: Computer.name is null or empty in Service.addComputer");
+		
+		computerDao.save(computer);
+		
+		Stat stat = new Stat(computer.getId(),"add");
+		
+		statDao.save(stat);
 	}
 
 	@Override
 	@Transactional(readOnly=false)
 	public void updateComputer(Computer computer) {
 		logger.debug("Entering Service.updateComputer");
-		try {
-			Assert.notNull(computer);
-			Assert.notNull(computer.getId());
-			Assert.hasText(computer.getName().trim());
-			
-			//Checking that the computer actually exists in the database
-			Assert.isTrue(computerDao.exists(computer.getId()));
-					
-			computerDao.save(computer);
-			
-			Stat stat = new Stat(computer.getId(),"update");
-			
-			statDao.save(stat);
-		} catch (IllegalArgumentException e) {
-			logger.warn("WARNING: iae in Service.updateComputer: {}", e.getMessage());
-			throw new IllegalArgumentException();
-		}
+		Assert.notNull(computer,"Assert Exception: Computer is null in Service.updateComputer");
+		Assert.notNull(computer.getId(),"Assert Exception: Computer.id is null in Service.updateComputer");
+		Assert.hasText(computer.getName(),"Assert Exception: Computer.name is null or empty in Service.updateComputer");
+		
+		//Checking that the computer actually exists in the database
+		Assert.isTrue(computerDao.exists(computer.getId()));
+				
+		computerDao.save(computer);
+		
+		Stat stat = new Stat(computer.getId(),"update");
+		
+		statDao.save(stat);		
 	}
 
 	@Override
 	@Transactional(readOnly=false)
 	public void deleteComputer(Long computerId) {
 		logger.debug("Entering Service.deleteComputer");
-		try {
-			Assert.notNull(computerId);
-			
-			Computer c = getComputer(computerId);
-			//Checking that the computer actually exists in the database
-			Assert.notNull(c);
-			
-			computerDao.delete(c);
-			
-			Stat stat = new Stat(computerId, "delete");
-			
-			statDao.save(stat);
-		} catch (IllegalArgumentException e){
-			logger.warn("WARNING: iae in Service.deleteComputer: {}", e.getMessage());
-			throw new IllegalArgumentException();
-		}
+		Assert.notNull(computerId,"Assert Exception: computerId is null in Service.deleteComputer");
+		
+		Computer c = getComputer(computerId);
+		//Checking that the computer actually exists in the database
+		Assert.notNull(c);
+		
+		computerDao.delete(c);
+		
+		Stat stat = new Stat(computerId, "delete");
+		
+		statDao.save(stat);
+		
 	}
 
 	@Override
 	public Computer getComputer(Long computerId) {
 		logger.debug("Entering Service.getComputer");		
-		Assert.notNull(computerId);
+		Assert.notNull(computerId,"Assert Exception: computerId is null in Service.getComputer");
 		
 		return computerDao.findOne(computerId);
 	}
@@ -126,11 +112,11 @@ public class ComputerDatabaseServiceImpl implements IComputerDatabaseService {
 	@Override
 	public Computers getComputers(Integer currentPage, Integer resultsPerPage, TableSort sortBy, String computerName, String companyName) {
 		logger.debug("Entering Service.getComputers: currentPage={} resultsPerPage={} sortBy={} searchComputerName={} searchCompanyName={}",currentPage,resultsPerPage,sortBy.getSortString(),computerName,companyName);
-		Assert.notNull(currentPage);
-		Assert.notNull(resultsPerPage);
-		Assert.notNull(sortBy);
-		Assert.notNull(computerName);
-		Assert.notNull(companyName);
+		Assert.notNull(currentPage,"Assert Exception: currentPage is null in Service.getComputers");
+		Assert.notNull(resultsPerPage,"Assert Exception: resultsPerPage is null in Service.getComputers");
+		Assert.notNull(sortBy,"Assert Exception: sortBy is null in Service.getComputers");
+		Assert.notNull(computerName,"Assert Exception: computerName is null in Service.getComputers");
+		Assert.notNull(companyName,"Assert Exception: companyName is null in Service.getComputers");
 		
 		Computers computers = null;
 		Pageable pageable = new PageRequest(currentPage-1, resultsPerPage, getSort(sortBy));
@@ -155,7 +141,7 @@ public class ComputerDatabaseServiceImpl implements IComputerDatabaseService {
 	@Override
 	public Company getCompany(Long companyId) {
 		logger.debug("Entering Service.getCompany");
-		Assert.notNull(companyId);
+		Assert.notNull(companyId,"Assert Exception: companyId is null in Service.getCompany");
 		return companyDao.findOne(companyId);
 	}
 
